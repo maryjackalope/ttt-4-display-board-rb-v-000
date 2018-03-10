08e8994350f6a838dad55add9812aa02b6e93725
Mary Jack <maryjackinthebox@gmail.com>
2018-03-10 01:24:00 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,10 +5,10 @@ board = [" "," "," "," "," "," "," "," "," "]
 
 def display_board(board)
 
-puts "#{board[1]}   |   |   \n"
+puts "#{board[0]}|#{board[1]}|#{board[2]} \n"
 puts "-----------\n"
-puts "   |   |   \n"
+puts "#{board[3]}|#{board[4]}|#{board[5]}  \n"
 puts "-----------\n"
-puts "   |   |   \n"
+puts "#{board[6]}|#{board[7]}|#{board[8]}  \n"
       
 end
